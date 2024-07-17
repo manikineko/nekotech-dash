f710dce7c37cfc005c5ce2bb8775c350991b8f2d
Daisy <66263341+bloomsirenix@users.noreply.github.com>
2024-07-17 15:54:22 +0200

    Update Terminal.jsx

--- src/Pages/Terminal.jsx
+++ src/Pages/Terminal.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { VncScreen } from 'react-vnc';
 
 const Terminal = ({ containerId }) => {
   const vncUrl = `ws://de-prem-01.hosts.optikservers.com:35300/vnc/${containerId}`;
+  const vncRef = useRef(null);
+
+  useEffect(() => {
+    if (vncRef.current) {
+      vncRef.current.focus();
+    }
+  }, []);
 
   const handleConnect = () => {
     console.log('VNC connected');
@@ -24,15 +31,15 @@ const Terminal = ({ containerId }) => {
         style={{ width: '100%', height: '500px' }}
         background="#000000"
         scaleViewport
+        viewOnly={false}
+        rfbOptions={{ credentials: { password: 'your_password' } }}
         onConnect={handleConnect}
         onDisconnect={handleDisconnect}
         onError={handleError}
+        ref={vncRef}
       />
     </div>
   );
 };
 
 export default Terminal;
-
-
-// do <url>/servers?con=<container id>
